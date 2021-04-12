ce4d98b712ae47a42c042113965a87b214e2ce1e
MahdiA64-WD <78160439+MahdiA64-WD@users.noreply.github.com>
2021-04-12 21:08:42 +0100

    Update connection.php

--- webroot/connection.php
+++ webroot/connection.php
@@ -1,11 +1,11 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "login_sample_dtb";
+$dbhost = "MYSQL_SERVICE_HOST";
+$dbuser = "DATABASE_USER";
+$dbpass = "DATABASE_PASSWORD";
+$dbname = "ecs417";
 
-if(!$con = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname))
+if(!$con = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname))
 {
 
 	die("failed to connect!");
